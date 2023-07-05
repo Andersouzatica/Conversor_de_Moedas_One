dbbaf85e2638e94dab02c160afb4bd2e1db67294
unknown <analista.suporte.imip@gmail.com>
2023-07-04 23:44:22 -0300

    Caixa de diálogo - Deseja continuar?

--- src/entity/Conversor.java
+++ src/entity/Conversor.java
@@ -16,42 +16,49 @@ public class Conversor extends JDialog {
 				"De Euros para Reais" };
 		BigDecimal cotacao_dotar = BigDecimal.valueOf(4.81); // ----03/07/2023
 		BigDecimal cotacao_euro = BigDecimal.valueOf(5.25); // -----03/07/2023
+		Integer escolha = 0;
+		do {
 		String jcb = (String) JOptionPane.showInputDialog(null, "Escolha uma opção", "Menu", JOptionPane.PLAIN_MESSAGE,
 				null, msg, msg[0]);
 		System.out.println(jcb);
 		String vl = JOptionPane.showInputDialog("Insira um valor:");
 		Integer valor = Integer.parseInt(vl);
-		System.out.println(valor);
-
+			
+		System.out.println("Valor inserido: " + valor);
 		switch (jcb) {
 		case "Conversor de Moeda":
 			String selectedMoeda = (String) JOptionPane.showInputDialog(null, "Escolha a moeda que deseja converter.",
 					"Moedas", JOptionPane.PLAIN_MESSAGE, null, moeda, moeda[0]);
+				
 			switch (selectedMoeda) {
 			case "De Reais para Dólares":
 				BigDecimal cot1 = BigDecimal.valueOf(valor / cotacao_dotar.doubleValue()).setScale(2, BigDecimal.ROUND_HALF_EVEN);
-				System.out.println(cot1);
+				System.out.println("Valor convertido: R$ " + cot1);
+				JOptionPane.showMessageDialog(null, "O valor da conversão é de R$ " + cot1);
 				break;
 			case "De Reais para Euros":
 				BigDecimal cot2 = BigDecimal.valueOf(valor / cotacao_euro.doubleValue()).setScale(2, BigDecimal.ROUND_HALF_EVEN);
-				System.out.println(cot2);
+				System.out.println("Valor convertido: R$ " + cot2);
+				JOptionPane.showMessageDialog(null, "O valor da conversão é de R$ " + cot2);
 				break;
 			case "De Dolares para Reais":
 				BigDecimal cot3 = BigDecimal.valueOf(valor * cotacao_dotar.doubleValue()).setScale(2, BigDecimal.ROUND_HALF_EVEN);
-				System.out.println(cot3);
+				System.out.println("Valor convertido: R$ " + cot3);
+				JOptionPane.showMessageDialog(null, "O valor da conversão é de R$ " + cot3);
 				break;
 			case "De Euros para Reais":
 				BigDecimal cot4 = BigDecimal.valueOf(valor * cotacao_euro.doubleValue()).setScale(2, BigDecimal.ROUND_HALF_EVEN);
-				System.out.println(cot4);
+				System.out.println("Valor convertido: R$ " + cot4);
+				JOptionPane.showMessageDialog(null, "O valor da conversão é de R$ " + cot4);
 				break;
 			}
-			break;
-		case "Conversor de Temperatura":
-			System.out.println(valor + 10);
-			break;
-
-		default:
-			break;
+		}
+		escolha = JOptionPane.showConfirmDialog(null, "Desejar continuar ?", "Pergunta ao Usuario", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE);
+		} while ( escolha == 0);
+		if(escolha == 1) {
+			JOptionPane.showMessageDialog(null, "Programa finalizado");
+		} else if (escolha == 2) {
+			JOptionPane.showMessageDialog(null, "Programa concluido");
 		}
 	}
 
